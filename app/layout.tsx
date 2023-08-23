47e634e9357bba8922d50ac30e3fbdd69eb8d1fc
Atif Dastgir <atif.dastgir@gmail.com>
2023-08-23 21:34:44 +0500

    Add meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import { Providers } from "@/app/providers";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "mindcusp",
-  description: "",
+  description:
+    "We are a team of developers who are passionate about crafting scalable web applications. We strive to help early stage startups in building and scaling their MVPs. Our team partners with you right from the start to ensure a fast time-to-market for your business idea.",
 };
 
 export default function RootLayout({
