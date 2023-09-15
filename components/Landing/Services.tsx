b92732005504aa833f839153b293ff829f266acc
Atif Dastgir <atif.dastgir@gmail.com>
2023-09-15 22:34:27 +0500

    modified services icons color

--- components/Landing/Services.tsx
+++ components/Landing/Services.tsx
@@ -12,25 +12,25 @@ function Services() {
       description:
         "We collaborate with the leadership team to brainstorm, design,\
       and create products that cater to the business needs, keeping in check the time to market.",
-      image: <IconSourceCode className="w-8 h-8" />,
+      image: <IconSourceCode className="w-8 h-8 text-primary-500" />,
     },
     {
       title: "Architecture & Design",
       description:
-        "Our team, consisting of experienced architects and engineering managers, is available to assist you in crafting your upcoming project, addressing both its technical and execution aspects.",
-      image: <IconBinaryTree2 className="w-8 h-8" />,
+        "Our team, consisting of experienced architects and engineering managers is available to assist you in crafting your upcoming project, addressing both its technical and execution aspects.",
+      image: <IconBinaryTree2 className="w-8 h-8 text-primary-500" />,
     },
     {
       title: "Software Consultancy",
       description:
         "Receive guidance from a consulting team comprising seasoned architects and developers with over 15 years of collective experience collaborating with startups, small to medium-sized businesses, and large enterprises.",
-      image: <IconAnalyze className="w-8 h-8" />,
+      image: <IconAnalyze className="w-8 h-8 text-primary-500" />,
     },
     {
       title: "Partnership",
       description:
-        "Our team possesses extensive expertise in establishing software developer teams from scratch, dedicated to delivering high-quality code while adhering to industry best practices. Collaborate with us to bring your next innovation to life!",
-      image: <IconHeartHandshake className="w-8 h-8" />,
+        "Our team possesses extensive expertise in establishing software developer teams from scratch, dedicated to delivering high-quality code while adhering to industry best practices. Partner with us to bring your next innovation to life!",
+      image: <IconHeartHandshake className="w-8 h-8 text-primary-500" />,
     },
   ];
 
